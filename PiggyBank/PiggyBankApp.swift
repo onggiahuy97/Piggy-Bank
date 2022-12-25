4e7b5a605d33f801d1dfb5752d32d975db3cde53
Huy Ong <huy.ong@sjsu.edu>
2022-12-25 14:55:47 -0800

    Initial Commit

---
+++ PiggyBank/PiggyBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyBankApp.swift
+//  PiggyBank
+//
+//  Created by Huy Ong on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct PiggyBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
